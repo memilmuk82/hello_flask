cf6eac3ec0faf5f5f9248dffb9afcd8bf5533313
Jinseon Lee <memilmuk82@gmail.com>
2026-04-01 08:36:29 +0900

    데이터를 HTML 화면에 표시

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # 이제는 JSON이 아니라 HTML 파일을 보냅니다
-    return render_template('index.html')
+		# 여기에 내 데이터를 만든다!
+    my_profile = {
+        "name": "홍길동", # 내 이름으로
+        "age": 18, # 내 나이로
+        "school": "종로산업정보학교",   # 추가!
+        "hobby": "게임"           # 추가!
+    }
+
+    # 이 부분이 바뀌었다!
+    return render_template('index.html', data=my_profile)
 
 if __name__ == '__main__':
     app.run(debug=True)
