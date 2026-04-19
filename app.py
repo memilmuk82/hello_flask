acea148e63f4fe6284b39ce7af509e67ebaf91b5
Jinseon Lee <memilmuk82@gmail.com>
2026-04-20 07:50:03 +0900

    삭제 기능 및 확장 구현

--- app.py
+++ app.py
@@ -12,10 +12,30 @@ def home():
 def send():
     skill = request.form.get('skill')
     level = request.form.get('level')
-    status = request.form.get('status')
-    
-    messages.append(status + " / " + level + " / " + skill)
-    
+    status = request.form.get('status')    
+    messages.append(status + " / " + level + " / " + skill)    
+    return redirect('/')
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    index = int(request.form['index'])
+    messages.pop(index)
+    return redirect('/')
+
+@app.route("/delete_all", methods=['POST'])
+def delete_all():
+    messages.clear()    
+    return redirect('/')
+
+@app.route('/delete_selected', methods=['POST'])
+def delete_selected():
+    indexes = request.form.getlist('delete_indexes')
+    indexes = [int(index) for index in indexes]
+    indexes.sort(reverse=True)
+
+    for index in indexes:
+        messages.pop(index)
+
     return redirect('/')
 
 if __name__ == '__main__':
